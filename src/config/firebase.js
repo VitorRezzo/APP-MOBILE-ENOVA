078e5b47cc60939c0742b1439eb4cd39cdf74109
VitorRezzo <cv.rezz99@gmail.com>
2021-09-21 10:24:51 -0300

    MANIPULANDO API FIREBASE config DB

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,12 +4,12 @@ import "firebase/storage";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDOximofctnzs6dPkl8zmp_L4mrZmQ76nI",
-  authDomain: "enovaenergia-multitarefa.firebaseapp.com",
-  projectId: "enovaenergia-multitarefa",
-  storageBucket: "enovaenergia-multitarefa.appspot.com",
-  messagingSenderId: "725961065739",
-  appId: "1:725961065739:web:52d74526476f6c5b384e0e"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
  firebase.initializeApp(firebaseConfig);
